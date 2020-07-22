2669a5116ac45bd0c8a05a1b6279bc4013955702
Ayuka-Okada <a.snow.rsc2@gmail.com>
2020-07-22 16:04:16 +0900

    write down Update function

--- app/Http/Controllers/Maternity_checkupController.php
+++ app/Http/Controllers/Maternity_checkupController.php
@@ -73,7 +73,10 @@ class Maternity_checkupController extends Controller
      */
     public function edit($id)
     {
-        //
+        // dd($id);
+        $maternity_checkup = Maternity_checkup::find($id); 
+
+        return view('maternity_checkups.edit');
     }
 
     /**
@@ -86,6 +89,22 @@ class Maternity_checkupController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $maternity_checkup = Maternity_checkup::find($id);
+
+        $maternity_checkup->date = $request->date;
+        $maternity_checkup->pregnancy_week = $request->pregnancy_week;
+        $maternity_checkup->fundal_height = $request->fundal_height;
+        $maternity_checkup->weight = $request->weight;
+        $maternity_checkup->abdominal_circumference = $request->abdominal_circumference;
+        $maternity_checkup->blood_pressure = $request->blood_pressure;
+        $maternity_checkup->edema = $request->edema;
+        $maternity_checkup->urinal_protein = $request->urinal_protein;
+        $maternity_checkup->urinal_sugar = $request->urinal_sugar;
+        $maternity_checkup->note = $request->note;
+        $maternity_checkup->user_id = 1;
+        $maternity_checkup->save(); //DBに保存
+
+    return redirect()->route('maternity_checkups.index'); 
     }
 
     /**
---
+++ resources/views/maternity_checkups/edit.blade.php
@@ -0,0 +1,58 @@
+<!-- 妊婦健診の登録・編集画面 -->
+@extends('form_layout')
+
+@section('title')
+妊婦健診
+@endsection
+
+@section('subtitle')
+検診結果
+@endsection
+
+@section('form')
+<form action="{{route('maternity_checkups.update')}}" class="" method="post">
+@endsection
+
+@section('lists')
+<li>検査年月日: <span><input type="date" name="date" id="date" value="{{ old('date', $maternity_checkup->date) }}"></span></li>
+<li>妊娠週数: <span><input type="number" name="pregnancy_week" id="pregnancy_week" value="{{ old('pregnancy_week', $maternity_checkup->pregnancy_week) }}"></span> 週</li>
+<li>子宮底調: <span><input type="number" name="fundal_height" id="fundal_height" value="{{ old('fundal_height', $maternity_checkup->fundal_height) }}"></span> cm</li>
+<li>体重: <span><input type="text" name="weight" id="weight" value="{{ old('weight', $maternity_checkup->weight) }}"></span> ㎏</li>
+<li>腹囲: <span><input type="number" name="abdominal_circumference" id="abdominal_circumference" value="{{ old('abdominal_circumference', $maternity_checkup->abdominal_circumference) }}"></span> cm</li>
+<li>血圧: <span><input type="number" name="blood_pressure" id="blood_pressure" value="{{ old('blood_pressure', $maternity_checkup->blood_pressure) }}"></span></li>
+<li>浮腫:
+    <span>
+        <select name="edema" id="edema" value="{{ old('edema, $maternity_checkup->edema) }}">
+            <option value="none"></option>
+            <option value="mainus">-</option>
+            <option value="plus">+</option>
+            <option value="two_plus">++</option>
+        </select>
+    </span>
+</li>
+<li>尿蛋白:
+    <span>
+        <select name="urinal_protein" >
+            <option value="none"></option>
+            <option value="mainus">-</option>
+            <option value="plus">+</option>
+            <option value="two_plus">++</option>
+        </select>
+    </span>
+</li>
+<li>尿糖:
+    <span>
+        <select name="urinal_sugar" >
+            <option value="none"></option>
+            <option value="mainus">-</option>
+            <option value="plus">+</option>
+            <option value="two_plus">++</option>
+        </select>
+    </span>
+</li>
+<li>備考欄: <span><input type="textarea" name="note" id="note">{{ old('note', $maternity_checkup->note) }}</span></li>
+@endsection
+
+@section('back')
+<a href="" class="back">更新</a>
+@endsection
