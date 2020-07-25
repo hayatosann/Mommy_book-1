8418834c4f99648e55a122ab240cd742ff22a953
Chiho99 <chihomikami.81@gmail.com>
2020-07-25 12:51:21 +0900

    modify

--- resources/views/vaccination.blade.php
+++ resources/views/vaccination.blade.php
@@ -14,20 +14,31 @@
 @endsection
 
 @section('lists')
-<li>ワクチン名:<!-- ワクチン名 --></li>
-<li>接種年月日:<!-- 接種年月日 --></li>
-<li>注射部位:<!-- 注射部位 --></li>
-<li>実施者名:<!-- 実施者名 --></li>
-<li>反応:<!-- 反応 -->mm</li>
-<li>判定:<!-- 判定 --></li>
-<li>病院名:<!-- 病院名 --></li>
-<li>メーカー/ロット:<!-- メーカー・ロット --></li>
-<li>備考:<!-- 備考 --></li>
+<div class="wrapper_display">
+    <div class="wrapper_lists">
+        <ul class="lists_title">
+            <li>ワクチン名:<!-- ワクチン名 --></li>
+            <li>接種年月日:<!-- 接種年月日 --></li>
+            <li>注射部位:<!-- 注射部位 --></li>
+            <li>実施者名:<!-- 実施者名 --></li>
+            <li>反応:<!-- 反応 -->mm</li>
+            <li>判定:<!-- 判定 --></li>
+            <li>病院名:<!-- 病院名 --></li>
+            <li>メーカー/ロット:<!-- メーカー・ロット --></li>
+            <li>備考:<!-- 備考 --></li>
+        </ul>
+        <div class="btn_edit">
+            <!-- 編集ボタン -->
+            <a href="" class="edit">編集</a>
+            <!-- 削除ボタン -->
+            <form action="" method="">
+                    <button class="delete">削除</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
 
-@section('edit')
-<a href="" class="edit">編集</a>
-@endsection
 
 @section('back')
 <a href="" class="back">戻る</a>
