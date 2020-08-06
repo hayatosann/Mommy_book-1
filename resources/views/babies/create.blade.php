6297a3268ff106b5e8e98b85107135937b959892
Chiho99 <chihomikami.81@gmail.com>
2020-08-06 17:06:26 +0900

    modify

--- resources/views/babies/create.blade.php
+++ resources/views/babies/create.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('/assets/css/style.css')}}">
 
     <title>おなかの赤ちゃん＆お子様（ご兄弟）のプロフィールご登録</title>
